3719d04da0387413a3a999e3cec8f1b12ac16270
Adham Ahmad <adhamahmad@outlook.com>
2023-08-12 22:41:35 +0200

    2

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,9 +1,21 @@
 import server from "./server";
+import {secp256k1} from "ethereum-cryptography/secp256k1"
+import {keccak256} from "ethereum-cryptography/keccak.js"
+import {bytesToHex as toHex} from "ethereum-cryptography/utils"
+import toHash from "./Functions/toHash";
+import daWallet from "./Functions/daWallet";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey}) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+    const publicKey = secp256k1.getPublicKey(privateKey, false);
+    const address = toHex(keccak256(publicKey.slice(1)).slice(-20));
     setAddress(address);
+    const message = "Take that";
+    const msgHash = toHash(message);
+    //const signature = secp256k1.sign(msgHash, privateKey, 1);
+    
     if (address) {
       const {
         data: { balance },
@@ -13,16 +25,15 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       setBalance(0);
     }
   }
-
   return (
     <div className="container wallet">
-      <h1>Your Wallet</h1>
-
+      <h1>Your Private Key</h1>
+      <div>Wallet Address: {address}</div>
+      <div className="errormsg">{invalidPK || null}</div>
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="" value={privateKey} onBlur={onChange} onChange={onChange}></input>
       </label>
-
       <div className="balance">Balance: {balance}</div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,25 +2,31 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const {secp256k1} = require('./node_modules/ethereum-cryptography/secp256k1');
+//const toHash = require('../Functions/toHash');
+//import {wallet} from '../client/src/Wallet';
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "b9929b97094bd27bff0b0a7c487f96ab255adeb8": 100, //beb48b84f0d5876a231f4a39d670891bd5ccd0ef9d921db276ea191187b6566c
+  "9a7839e830ffbcef723de201fe26e40293fa9a70": 50, //b781b6556ee13a81270698575f78862c53553d1c8af732e97567230c79088329
+  "cd1d93e94f2bbe2bf3a7162db17c1dcf1eb9baf3": 75, //461e07eefe4d3928d3085ed26833ea190bf88faf52c223f35151f5680d3289e1
 };
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
+  
   const balance = balances[address] || 0;
   res.send({ balance });
 });
 
 app.post("/send", (req, res) => {
+  //TODO : get a signature from the client side application
+  //recover the public address from the signature
+  
   const { sender, recipient, amount } = req.body;
-
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
@@ -39,6 +45,6 @@ app.listen(port, () => {
 
 function setInitialBalance(address) {
   if (!balances[address]) {
-    balances[address] = 0;
+    balances[address] = 50;
   }
 }
